4deca08da2722576bcbd213e520f220a2d184b17
Marc Siegel <marc@usainnov.com>
2013-07-25 18:46:34 -0400

    (cosmetic: wrap long lines in docile.rb)

--- lib/docile.rb
+++ lib/docile.rb
@@ -22,10 +22,14 @@ module Docile
     block_context = eval("self", block.binding)
     proxy_context = FallbackContextProxy.new(dsl, block_context)
     begin
-      block_context.instance_variables.each { |ivar| proxy_context.instance_variable_set(ivar, block_context.instance_variable_get(ivar)) }
+      block_context.instance_variables.each do |ivar|
+        proxy_context.instance_variable_set(ivar, block_context.instance_variable_get(ivar))
+      end
       proxy_context.instance_exec(*args,&block)
     ensure
-      block_context.instance_variables.each { |ivar| block_context.instance_variable_set(ivar, proxy_context.instance_variable_get(ivar)) }
+      block_context.instance_variables.each do |ivar|
+        block_context.instance_variable_set(ivar, proxy_context.instance_variable_get(ivar))
+      end
     end
     dsl
   end
